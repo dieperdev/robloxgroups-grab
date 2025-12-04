8eda81f7aa6b6e8d3efa95a8b4723edb873f8e20
dieperdev <140034404+dieperdev@users.noreply.github.com>
2025-12-03 21:32:33 -0500

    add group featured content item type

--- pipeline.py
+++ pipeline.py
@@ -268,6 +268,9 @@ class WgetArgs(object):
             elif item_type == 'group-shout':
                 wget_args.extend(['--warc-header', 'robloxgroups-api-shout: '+item_value])
                 wget_args.append('https://apis.roblox.com/community-links/v1/groups/{}/shout'.format(item_value))
+            elif item_type == 'group-featuredcontent':
+                wget_args.extend(['--warc-header', 'robloxgroups-api-featuredcontent: '+item_value])
+                wget_args.append('https://groups.roblox.com/v1/featured-content/event?groupId={}'.format(item_value))
             elif item_type == 'group-namehistory':
                 group_id, cursor = item_value.split(':', 1)
                 wget_args.extend(['--warc-header', 'robloxgroups-api-namehistory: '+item_value])
